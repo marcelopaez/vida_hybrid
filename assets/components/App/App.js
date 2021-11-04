d80184fb16080a38e8651f82bd71e023012c78c2
facufrastas <facufrastas@gmail.com>
2021-11-04 13:41:23 -0300

    Reorganización de componentes.

--- assets/components/App/App.js
+++ assets/components/App/App.js
@@ -15,7 +15,7 @@ const App = () => {
       {/* <NavbarAdmin /> */}
       <Navbar />
       <Switch>
-        <Route exact path="/app" component={Planes} />
+        <Route exact path="/app" component={Home} />
         <Route exact path="/reservas" component={Reservas} />
         <Route exact path="/planes" component={Planes} />
         <Route exact path="/contrato" component={Contrato} />
