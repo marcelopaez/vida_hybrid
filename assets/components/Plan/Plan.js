850508ffd1cbb96cf5465cf55870e2ad3f23e096
Facundo <facufrastas@gmail.com>
2021-11-25 16:04:35 -0300

    Avances del Home.

--- assets/components/Plan/Plan.js
+++ assets/components/Plan/Plan.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
-const Plan = ({ svg, title, description, full, part, background }) => {
+const Plan = ({ svg, title, description, full, part, background, price }) => {
   return (
     <div className={background ? 'planContainer' : 'planContainerWithBackground'}>
       {svg}
-      <p className="planContainerTitle">{title}</p>
-      <p className="planContainerDescription">
-        <b>{description}</b>
-        <br />
-        FULL: {full} <br />
-        PART: {part}
+      <p className="planContainerTitle extraBoldFont">{title}</p>
+      <p className="planContainerDescription lightFont">
+        {description}
+        {full?.length > 0 ? (
+          <>
+            <br />
+            FULL: {full} <br />
+            PART: {part}
+          </>
+        ) : null}
       </p>
-      <button className="planContainerButton">Suscribite</button>
+      {price?.length > 0 ? <p className="planContainerPrice extraBoldFont">${price}</p> : null}
     </div>
   );
 };
--- assets/components/Service/Service.js
+++ assets/components/Service/Service.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Service = ({ svg, title, subtitle, small }) => {
   return (
     <div className={!small ? 'serviceContainer' : 'smallServiceContainer'}>
-      <div className="serviceContainerImage">{svg}</div>
-      <div className="serviceContainerInformation">
+      <div className={!small ? 'serviceContainerImage' : 'smallServiceContainerImage'}>{svg}</div>
+      <div className={!small ? 'serviceContainerInformation' : 'smallServiceContainerInformation'}>
         <p className="boldFont serviceTitle">{title}</p>
-        <p>{subtitle}</p>
+        <p className="serviceSubtitle">{subtitle}</p>
       </div>
     </div>
   );
